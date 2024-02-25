58f733e82ba0d5020ca888c2f4a19f19b31cbdbf
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2024-02-25 15:25:27 +0200

    Add hamburger menu btn

--- js/main.js
+++ js/main.js
@@ -8,3 +8,11 @@ function onLinkSelected(elLink) {
   const elBox = elLink.closest('li')
   elBox.classList.add('selected')
 }
+
+function onToggleMenu() {
+  document.body.classList.toggle('menu-open')
+
+  const elMenuBtn = document.querySelector('.btn-menu')
+  const str = document.body.classList.contains('menu-open') ? 'X' : '☰'
+  elMenuBtn.innerText = str
+}
